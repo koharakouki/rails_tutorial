91eab4703623e29381b2fd4d19b4fa0644b7ad4e
koharakouki <ko.kohara02@gmail.com>
2020-03-31 12:46:00 +0900

    add micropost-search

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,13 +1,15 @@
 class MicropostsController < ApplicationController
 before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
+
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
-      @feed_items = []
+      @q = Micropost.none.ransack
+      @feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
     end
   end
